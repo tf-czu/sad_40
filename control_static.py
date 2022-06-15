fac875d243ecd7e08f16604a2c482ba04a78f001
tajgr <levjakub@gmail.com>
2022-06-15 16:15:37 +0200

    control_static.py: add reading client response

--- control_static.py
+++ control_static.py
@@ -2,7 +2,8 @@
     Testing control app
 """
 
-from zmq_node import push_msg
+import time
+from zmq_node import push_msg, pull_msg
 
 
 def control_main():
@@ -10,6 +11,14 @@ def control_main():
         label = input("Enter label: ")
         print(f"Sending: {label}")
         push_msg(label)
+
+        while True:
+            response = pull_msg()
+            if response:
+                print(response[0])
+                break
+
+            time.sleep(0.5)
 
 if __name__ == "__main__":
     control_main()
