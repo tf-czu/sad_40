f40b9f15588f9d1682a0e398df068b734b7790a0
tajgr <levjakub@gmail.com>
2022-06-15 11:00:29 +0200

    measure_node.py: add make_dir based on label

--- measure_node.py
+++ measure_node.py
@@ -1,22 +1,28 @@
 """
     TODO
 """
+import os.path
 
 from osgar.node import Node
 from osgar.bus import BusShutdownException
-from datetime import timedelta
-
-def make_dir(label):
-    print(label)
-    return "some_dir"  # TODO
+import datetime
 
 
 class MeasureNode(Node):
     def __init__(self, config, bus):
         super().__init__(config, bus)
-        bus.register('workin_dir')
+        bus.register('work_dir')
         self.control_msg = None
         self.bus = bus
+        self.storage_path = config.get("storage_path", "/home/ovosad/ovosad_data")
+        self.wdn1 = datetime.datetime.now().strftime("%y%m%d") # day working dir
+
+    def make_dir(self, label):
+        wdn2 = f"{datetime.datetime.now().strftime('%H%M%S')}_{label}"
+        work_dir_name = os.path.join(self.storage_path, self.wdn1, wdn2)
+        os.makedirs(work_dir_name)
+
+        return work_dir_name
 
 
     def run(self):
@@ -24,8 +30,8 @@ class MeasureNode(Node):
             while self.is_bus_alive():
                 self.update()
                 if self.control_msg is not None:
-                    dir_name = make_dir(self.control_msg)
-                    self.publish("workin_dir", dir_name)
+                    work_dir_name = self.make_dir(self.control_msg)
+                    self.publish("work_dir", work_dir_name)
                     self.control_msg = None
 
         except BusShutdownException:
