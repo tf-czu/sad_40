86dfa4be32f9c62938ce4ac684f06ac50d1d024c
tajgr <levjakub@gmail.com>
2022-09-08 16:48:03 +0200

    test_measure_node.py: add test_process_image

--- test_measure_node.py
+++ test_measure_node.py
@@ -3,10 +3,12 @@ from unittest.mock import MagicMock
 
 from measure_node import MeasureNode
 import os
+import numpy as np
+import cv2
 
 class MeasureNodeTest(unittest.TestCase):
     def test_make_dir(self):
-        c = MeasureNode(config={"storage_path": "."}, bus=MagicMock() )
+        c = MeasureNode(config={"storage_path": "."}, bus=MagicMock())
         c.wdn1 = "tmp_test"
         wd = c.make_dir("test_label")
         print(wd)
@@ -14,6 +16,13 @@ class MeasureNodeTest(unittest.TestCase):
         self.assertTrue(os.path.isdir(wd))
         os.rmdir(wd)
         os.rmdir(os.path.join(c.storage_path, c.wdn1))
+
+
+    def test_process_image(self):
+        c = MeasureNode(config={}, bus=MagicMock())
+        test_color = np.ones((750, 1000, 3), dtype=np.uint8)
+        res = c.process_image(test_color)
+        self.assertTrue(cv2.imdecode(res, 1).shape == (450, 600, 3))
 
 
 if __name__ == '__main__':
