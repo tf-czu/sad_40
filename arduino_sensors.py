69cf34862a2b39df047e6788c9d2340aac8920e0
tajgr <levjakub@gmail.com>
2023-05-10 12:32:22 +0200

    arduino_sensors.py: initial commit, first test

---
+++ arduino_sensors.py
@@ -0,0 +1,32 @@
+"""
+    TODO
+"""
+import os
+from osgar.node import Node
+
+
+class ArduinoSensors(Node):
+    def __init__(self, config, bus):
+        super().__init__(config, bus)
+        bus.register('trig')
+        self.bus = bus
+        self.work_dir = None
+        self.buf = b""
+
+    def save_data(self, data):
+        assert self.work_dir is not None
+        with open(os.path.join(self.work_dir, "arduino_sensors.txt"), "w") as f:
+            f.write(data)
+
+    def on_work_dir(self, data):
+        self.work_dir = data
+        self.publish("trig", b"\r\n")
+        self.sleep(1)
+
+    def on_raw(self, data):
+        if self.work_dir:
+            self.buf += data
+            if b"/n" in self.buf:
+                self.save_data(self.buf.decode())
+                self.work_dir = None
+                self.buf = b""
