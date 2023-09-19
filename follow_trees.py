a02c91e95b5ba3e4561233dab7a072b45544b9d3
tajgr <levjakub@gmail.com>
2023-09-19 14:13:37 +0200

    follow_trees.py: remove scan tranformtion

--- follow_trees.py
+++ follow_trees.py
@@ -2,7 +2,6 @@
     TODO
 """
 import math
-import numpy as np
 
 from osgar.node import Node
 from osgar.explore import follow_wall_angle
@@ -14,25 +13,19 @@ class FollowTrees(Node):
         super().__init__(config, bus)
         bus.register("desired_speed", "scan")
         self.bus = bus
-        self.max_speed = config.get("max_speed", 0.5)
+        self.max_speed = config.get("max_speed", 0.3)
         self.scan = None
 
-    def scan_transform(self, scan):  # rotate lidar by 90 deg, it could be removed in the future
-        scan = np.array(scan)
-        ret_scan = np.zeros_like(scan)
-        ret_scan[:405] = scan[270:675]
-        return ret_scan
-
     def send_speed(self, speed, desired_direction):
-        return self.publish('desired_speed', [round(speed * 1000), round(math.degrees(desired_direction) * 100)])
+        return self.publish('move', [round(speed * 1000), round(math.degrees(desired_direction) * 100)])
 
     def go_safely(self, desired_direction):
         # TODO stop if a obstacle is too close.
         self.send_speed(self.max_speed, desired_direction)
 
     def on_scan(self, data):
-        self.scan = self.scan_transform(data)
-        desired_direction = normalizeAnglePIPI(follow_wall_angle(self.scan, gap_size=3, wall_dist=1, right_wall=True))
-        self.publish("scan", self.scan)
+        self.scan = data
+        desired_direction = normalizeAnglePIPI(follow_wall_angle(self.scan, gap_size=3, wall_dist=1.5, right_wall=True))
+        assert desired_direction is None
         self.go_safely(desired_direction)
 
