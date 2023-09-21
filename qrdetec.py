8ff2dc8139f093e12b76ba0f3b0b4959c7042c02
tajgr <levjakub@gmail.com>
2023-09-21 13:59:00 +0200

    qrdetec.py: add subsample

--- qrdetec.py
+++ qrdetec.py
@@ -12,12 +12,16 @@ class QrDetec(Node):
         super().__init__(config, bus)
         bus.register('qr_data')
         self.bus = bus
+        self.counter = 0
         self.verbose = False
 
     def on_image(self, data):
-        frame = cv2.imdecode(np.frombuffer(data, dtype=np.uint8), 1)
-        qr_data = decode(frame)
-        if self.verbose:
-            print(qr_data, frame.shape)
-        if qr_data:
-            self.publish("qr_data", qr_data)
+        self.counter += 1
+        if self.counter == 2:
+            self.counter = 0
+            frame = cv2.imdecode(np.frombuffer(data, dtype=np.uint8), 1)
+            qr_data = decode(frame)
+            if self.verbose:
+                print(qr_data, frame.shape)
+            if qr_data:
+                self.publish("qr_data", qr_data)
