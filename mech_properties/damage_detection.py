35f30676628f51f801574ca3156f8aa2c754ff2a
Jakub Lev <levjakub@gmail.com>
2024-03-14 15:42:14 +0100

    damage_detection.py: add radius measurement (#15)

--- mech_properties/damage_detection.py
+++ mech_properties/damage_detection.py
@@ -5,6 +5,7 @@
 import csv
 import os
 import datetime
+import math
 
 import cv2
 import numpy as np
@@ -60,28 +61,29 @@ def get_circle(p1, p2, p3):
 
 
 def calculate_areas(points, im):
-    ret = []
-    # for ii in range(2):
-    for ii in range(1):
-        im1 = np.zeros_like(im[:, :, 0])
-        im2 = np.zeros_like(im[:, :, 0])
-        im3 = np.zeros_like(im[:, :, 0])
-        p1, p2, p3, p4 = points[ii*4:4*(ii+1)]  # border points p1 and p3, apple skin p2, damage peak p4
-        # Circle on apple skin:
-        cx, cy, r = get_circle(p1, p2, p3)
-        cv2.circle(im1, (cx, cy), r, 255, -1)
-        # Circle on apple skin:
-        cx, cy, r = get_circle(p1, p4, p3)
-        cv2.circle(im2, (cx, cy), r, 255, -1)
-        mask = np.logical_and(im1!=0, im2!=0)
-        contours, _ = cv2.findContours(mask.astype(np.uint8), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-        assert len(contours) == 1
-        cnt = contours[0]
-        area = cv2.contourArea(cnt)
-        color = np.mean(im[mask], 0)
-        ret.append([area, cnt, color])
+    if len(points) == 3:  # return just radius
+        cx, cy, r = get_circle(*points)
+        return r, cx, cy
 
-    return ret
+    assert len(points)==4, points
+    im1 = np.zeros_like(im[:, :, 0])
+    im2 = np.zeros_like(im[:, :, 0])
+    p1, p2, p3, p4 = points  # border points p1 and p3, apple skin p2, damage peak p4
+    # Circle on apple skin:
+    cx, cy, r = get_circle(p1, p2, p3)
+    cv2.circle(im1, (cx, cy), r, 255, -1)
+    # Circle on apple tissue:
+    cx, cy, r = get_circle(p1, p4, p3)
+    cv2.circle(im2, (cx, cy), r, 255, -1)
+
+    mask = np.logical_and(im1!=0, im2!=0)
+    contours, _ = cv2.findContours(mask.astype(np.uint8), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    assert len(contours) == 1
+    cnt = contours[0]
+    area = cv2.contourArea(cnt)
+    color = np.mean(im[mask], 0)
+
+    return [area, cnt, color]
 
 
 class ImageProcessing:
@@ -96,8 +98,11 @@ class ImageProcessing:
             print(x, y)
             self.last_mpoint = (x, y)
 
-
-    def image_processing(self, data):
+    def image_processing(self, data, roundness=False):
+        if roundness:
+            num_points = 3
+        else:
+            num_points = 4
         ii = 0
         cv2.namedWindow("win", cv2.WINDOW_NORMAL)
         cv2.resizeWindow("win", 1068, 712)
@@ -112,26 +117,35 @@ class ImageProcessing:
             for p in points:
                 cv2.drawMarker(im_to_show, p, (255, 0, 0), cv2.MARKER_CROSS, markerSize=30, thickness=6)
             cv2.putText(im_to_show, f"{label}, {im_id}", (50, 100), cv2.FONT_HERSHEY_SIMPLEX, fontScale=3, color=(255, 0, 0), thickness=5)
-            for r in res:
-                area, cnt, __ = r
-                cv2.drawContours(im_to_show, [cnt], -1, (255, 0, 0), 4)
+            if res:
+                if roundness:
+                    __, radius_px, cx, cy = res
+                    cv2.circle(im_to_show, (cx, cy), radius_px, (255, 0, 0), 4)
+                else:
+                    area, cnt, __ = res
+                    cv2.drawContours(im_to_show, [cnt], -1, (255, 0, 0), 4)
 
             cv2.imshow("win", im_to_show)
             cv2.setMouseCallback('win', self.manual_label)
 
             k = cv2.waitKey(100) & 0xFF
-            if self.last_mpoint and len(points) < 4:
+            if self.last_mpoint and len(points) < num_points:
                 points.append(self.last_mpoint)
                 self.last_mpoint = None
 
             if k == ord("c"):
-                if len(points) == 4:
+                if len(points) == num_points:
                     scale = get_scale(img)
                     print(scale)
                     # (area1, cnt1, color1), (area2, cnt2, color2) = calculate_areas(points, im_to_show)
-                    print(calculate_areas(points, im_to_show))
-                    area1, cnt1, color1 = calculate_areas(points, im_to_show)[0]
-                    res = [(area1*scale, cnt1, color1)]  # , (area2*scale, cnt2, color2)
+                    if roundness:
+                        print(calculate_areas(points, im_to_show))
+                    if roundness:
+                        radius_px, cx, cy = calculate_areas(points, im_to_show)
+                        res = [radius_px*math.sqrt(scale), radius_px, cx, cy]
+                    else:
+                        area1, cnt1, color1 = calculate_areas(points, im_to_show)
+                        res = [area1*scale, cnt1, color1]  # , (area2*scale, cnt2, color2)
 
             if k == ord("d"):  # delete points
                 points = []
@@ -153,7 +167,7 @@ class ImageProcessing:
         return data
 
 
-def main_processing(img_path, dict_path):
+def main_processing(img_path, dict_path, roundness=False):
     varieties_dict = get_varieties_dict(dict_path)
     data = []
     for label, variety in varieties_dict.items():
@@ -163,23 +177,33 @@ def main_processing(img_path, dict_path):
         variety_im_path = os.path.join(img_path, im_dir)
         print(variety_im_path)
         img_list = sorted(os.listdir(variety_im_path))
-        for ii, im_file in enumerate(img_list):
-            if label == "E" and ii == 0:
-                continue
+        for im_file in img_list:
+            try:
+                ii = int(im_file.split('_')[1].split(".")[0]) - 1
+            except Exception as e:
+                print(e)
+                assert False
+
             assert str(ii+1) in im_file, (ii, im_file)
             im = cv2.imread(os.path.join(variety_im_path, im_file))
             data.append([label, variety, ii, im, [], []])  # the last variables are for label points and results
 
     processing = ImageProcessing()
-    data = processing.image_processing(data)
+    data = processing.image_processing(data, roundness=roundness)
     csv_filename = datetime.datetime.now().strftime("apple_%y%m%d_%H%M%S.csv")
     with open(csv_filename, "w", newline='') as csv_file:
         csv_writer = csv.writer(csv_file, delimiter=",")
-        csv_writer.writerow(["label", "apple ID", "variety", "repetition", "area (mm2)", "r", "g", "b"])
-        for label, variety, im_id, img, points, res in data:
-            for ii, (area, cnt, (b, g, r)) in enumerate(res):
-                csv_writer.writerow([label, im_id, variety, ii+1, area, r, g, b])
-
+        if roundness:
+            csv_writer.writerow(["label", "apple ID", "variety", "Radius"])
+            for label, variety, im_id, img, points, radius_data in data:
+                if radius_data:
+                    csv_writer.writerow([label, im_id, variety, radius_data[0]])
+        else:
+            csv_writer.writerow(["label", "apple ID", "variety", "area (mm2)", "r", "g", "b"])
+            for label, variety, im_id, img, points, result in data:
+                if result:
+                    area, cnt, (b, g, r) = result
+                    csv_writer.writerow([label, im_id, variety, area, r, g, b])
 
 
 if __name__ == "__main__":
@@ -188,6 +212,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('--img_path', '-i', help='Path to images', default='images/')
     parser.add_argument('--dict', '-d', help='Path to varieties dict file.', default='varieties_dict.csv')
+    parser.add_argument('--roundness', help='Switch to roundness mode - Only sample roundness is determined.',
+                        action='store_true')
     args = parser.parse_args()
 
-    main_processing(args.img_path, args.dict)
+    main_processing(args.img_path, args.dict, roundness=args.roundness)
