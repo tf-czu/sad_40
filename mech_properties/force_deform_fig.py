c2a41551f2eea1bff59bdb01f8aaf61ff6ca8bfd
jlev <jlev@tf.czu.cz>
2025-07-22 13:13:26 +0200

    Add r2 calculation

--- mech_properties/force_deform_fig.py
+++ mech_properties/force_deform_fig.py
@@ -8,6 +8,38 @@ import numpy as np
 from press_processing import load_data, prepper_data
 
 
+def calculate_r2(y_true, y_pred):  # TODO move to stat tools
+    """
+    Calculate coefficient of determination (R^2).
+
+    Parameters:
+    y_true (numpy.ndarray)
+    y_pred (numpy.ndarray)
+
+    Return:
+    float: R^2.
+    """
+    y_true = np.asarray(y_true)
+    y_pred = np.asarray(y_pred)
+
+    # 1. Sum of Squares of Residuals
+    ss_res = np.sum((y_true - y_pred)**2)
+
+    # 2. Total Sum of Squares
+    y_true_mean = np.mean(y_true)
+    ss_tot = np.sum((y_true - y_true_mean)**2)
+
+    if ss_tot == 0:
+        if ss_res == 0:
+            return 1.0
+        else:
+            return 0.0
+
+    else:
+        r2 = 1 - (ss_res / ss_tot)
+        return r2
+
+
 def draw_fig(force, deform):
     fig, ax1 = plt.subplots(1, 1, figsize=(4, 4))
     ax1.plot(deform, force, "k.", label="Force")
@@ -21,8 +53,11 @@ def draw_fig(force, deform):
     a_id = np.argmin(abs(deform - x_a))
     coeff = np.polyfit(deform[:a_id], force[:a_id], 1)
     k_a, intercept = coeff
+    print(f"k_a: {k_a}, intercept: {intercept}")
     pa = np.poly1d(coeff)
     ax1.plot(deform[:a_id], pa(deform[:a_id]), "r--", label="Line")
+    r2  = calculate_r2(force[:a_id], pa(deform[:a_id]))
+    print(f"R2: {r2}")
 
     rear_id = np.argmin(abs(deform - (x_max + 1)))
     rear_average = np.mean(force[rear_id:])
